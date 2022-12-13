4c5b927b79c6728e8d3cb295c4ded209feebd188
febinfredi <58045099+febinfredi@users.noreply.github.com>
2022-12-13 14:48:13 -0500

    Add files via upload
    Added gradient calculation

--- src/statemachine/scripts/score.py
+++ src/statemachine/scripts/score.py
@@ -6,6 +6,7 @@ from RRT import RRT
 import time
 from matplotlib import cm
 import matplotlib.pyplot as plt
+import scipy
 
 class Score():
 	
@@ -15,45 +16,52 @@ class Score():
 		self.FILE_PATH = "/home/febin/c_ws/src/autonomous_tb/robot_maps/ground_truth_costmap.pgm"
 		# With obstacles
 		self.FILE_PATH2 = "/home/febin/c_ws/src/autonomous_tb/robot_maps/obs_costmap.pgm"
+		# Change detect map
+		self.FILE_PATH_CHANGE = "/home/febin/c_ws/src/autonomous_tb/robot_maps/change_map.png"
 		
 		# Goal coordinates 
 		self.goal  = (203, 275)
-		
-		# Draw map flag
-		self.draw_map = True 
 		# Start coordinates
 		self.start_coordinates = [(114, 394), (135, 151), (184, 130), (111, 300), (207, 372)]
 		self.name_map = {114:'Room 1', 135:'Room 2', 184:'Room 3', 111:'Room 4', 207:'Room 5'}
-		# Map scores without obstacles
-		self.map_scores_no_obs, self.gradient_map = self.score(self.start_coordinates, self.goal, self.FILE_PATH)
-		# Map scores with obstacles
-		self.map_scores_obs, self.foo = self.score(self.start_coordinates, self.goal, self.FILE_PATH2)
 		
+		# Draw map flag
+		self.draw_map = False 
+		
+		# Gradient parameters
+		self.kernel_size = 50
+		self.sigma = 0.6
+		self.max_cost = 10
+		
+		# -------------------Scores using Path length-------------------
+		# Scores for map without obstacles
+		self.map_scores_no_obs = self.path_score(self.start_coordinates, self.goal, self.FILE_PATH)
+		# Scores for map with obstacles
+		self.map_scores_obs = self.path_score(self.start_coordinates, self.goal, self.FILE_PATH2)
+		# Dictionary to store changes in path length
 		self.change_dict = dict()
-		
+		# Find perecentage change
 		for key in self.map_scores_obs.keys():
 			perc_change = ((self.map_scores_obs[key][0] - self.map_scores_no_obs[key][0]) / self.map_scores_no_obs[key][0])*100 
 			self.change_dict[self.name_map[key]] = perc_change	
-		
+		# Print path length scores 
 		print("Map scores (No obstacles):")		
-		print(self.map_scores_no_obs)
+		#print(self.map_scores_no_obs)
 		print(" ")
 		print("Map scores (With obstacles):")
-		print(self.map_scores_obs)
+		#print(self.map_scores_obs)
 		print(" ")
 		print("Percent change in each room:")
 		for key in self.change_dict.keys():
 			print("{}: {:0.2f}%".format(key, self.change_dict[key]))
 		
-		# For 3d surface plot
-		self.X = np.arange(0, 384)
-		self.Y = np.arange(0, 384)
-		self.X, self.Y = np.meshgrid(X, Y)
-		self.fig, self.ax = plt.subplots(subplot_kw={"projection": "3d"})
-		self.surf = self.ax.plot_surface(self.X, self.Y, self.gradient_map, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-		#plt.contour(self.gradient_map)
-		self.fig.colorbar(surf, shrink=0.5, aspect=5)
-		plt.show()	
+		# -------------------Scores using Gradient map-------------------	
+		self.lin_conv_obs = self.grad_score(self.start_coordinates, self.goal, self.FILE_PATH, self.FILE_PATH_CHANGE, self.kernel_size, self.sigma, self.max_cost)
+		# Print gardient map score
+		print("2D Convolution: ")
+		print(self.lin_conv_obs)
+			
+		
 
 	def load_map(self, file_path, resolution_scale):
 		''' Load map from an image and return a 2D binary numpy array
@@ -74,13 +82,35 @@ class Score():
 		# Result 2D numpy array
 		return map_array
 		
+	def convolve_linear(self, signal, filter, output_size):
+		"""
+		Linear convolution
+		"""
+		out = np.zeros(output_size)
+		sum = 0
+
+		for i in range(output_size[0]):
+			for j in range(output_size[1]):
+				for k in range(max(0, i-filter.shape[0]), i+1):
+					for l in range(max(0, j-filter.shape[1]), j+1):
+						#with suppress(IndexError):
+						sum += signal[k, l] * filter[i-k, j-l]
+				out[i, j] = sum
+				sum = 0
+
+		return out	
+		
 	def gkern(self, kernel_size, sigma=1, mu=0):
+		"""
+		Gaussian filter
+		"""
 		x, y = np.meshgrid(np.linspace(-1,1,kernel_size), np.linspace(-1,1,kernel_size))
 		d = np.sqrt(x*x+y*y)
 		return np.exp(-( (d-mu)**2 / ( 2.0 * sigma**2 ) ) )
 
-	def grad_map(self, map_array, risk_points, kernel_size, sigma, max_cost=10):
-		grad_map = map_array
+	def grad_map(self, grnd_trth_map_path, risk_points, kernel_size, sigma, max_cost=10):
+		# Load gradient_map
+		grad_map = self.load_map(grnd_trth_map_path, 1)
 		kernel = self.gkern(kernel_size=kernel_size, sigma=sigma)*max_cost
 		kernel_len = len(kernel)
 		steps = int(kernel_len/2.2)
@@ -96,22 +126,12 @@ class Score():
 					i+=1
 		return grad_map      	
 
-	def score(self, locations, goal, file_path):
-		"""
-		arguments : list containing tuple of start coordinates
-				  : goal_coordinates
-				  : file_path to the map
-		return    : dictionary{start_coordinate[0]: (path_length, path_nodes)} 
-		"""
+	def path_score(self, locations, goal, file_path):
 		# Load the map
 		map_array = self.load_map(file_path, 1)
-		risk_coordinates = [points for points in locations]
-		risk_coordinates.append(goal)
-		gradient_map = self.grad_map(map_array, risk_coordinates, kernel_size=50, sigma=0.6, max_cost=10)
-		gradient_map = np.flipud(gradient_map)
-		
 		start_time = time.time()
 		score_dict = dict()
+		# Run Informed RRT star 
 		for start_coord in locations:
 			# Planning class
 			RRT_planner = RRT(map_array, start_coord, goal, self.draw_map)
@@ -120,16 +140,29 @@ class Score():
 		end_time = time.time()
 		print("Total time for calculating scores is %.2f" %(end_time-start_time))
 		
-		return score_dict, gradient_map	
-			
+		return score_dict	
+		
+	def grad_score(self, locations, goal, grnd_trth_file_path, change_map_file_path, k_size, sig, m_cost):	
+		# Load change map
+		change_map_array = self.load_map(change_map_file_path, 1)
+		
+		# List containing all risk points and goal coordinates
+		risk_coordinates = [points for points in locations]
+		risk_coordinates.append(goal)
+		
+		# Find gradient map
+		gradient_map = self.grad_map(grnd_trth_file_path, risk_coordinates, kernel_size=k_size, sigma=sig, max_cost=m_cost)
+		gradient_map = np.flipud(gradient_map)	
+		# Crop gradient map
+		grad_map_cropped = gradient_map[:-8, :-7]
+		# 2D convolution of cropped gradient map and change map
+		l_conv = self.convolve_linear(change_map_array, grad_map_cropped, grad_map_cropped.shape)
+		
+		return(l_conv)
+		
 
 if __name__ == "__main__":
 	
 	Score()	
-	# start = (114, 95) # Room 1, Inf_RRT_star = 143
-	# start = (162, 73) # Room 2, Inf_RRT_star = 165
-	# start = (91, 246) # Room 4, Inf_RRT_star = 111
-	#start = (184, 321) # Room 5, Inf_RRT_star = 101
-	# Room 3
 	
 	
